451be2d183d864490bd306d3987e70fcce877495
Daniele Tabanella <daniele.tabanella@gmail.com>
2025-01-29 22:54:17 +0100

    Adjust ship movement speed and normalize sprite handling; improve update logic

--- lua/entities/ship.lua
+++ lua/entities/ship.lua
@@ -7,13 +7,12 @@ ship = entity:extend({
 
 	dx = 0,
 	dy = 0,
-	spd = 1,
+	spd = 1.4,
 
 	sprts = { 1, 3, 5, 7, 9 },
 	sprt = 0,
 
 	shotTemp = 0,
-	sprtTemp = 0,
 
 	fire = function(_ENV)
 		-- spawn bullet
@@ -30,22 +29,20 @@ ship = entity:extend({
 	update = function(_ENV)
 		dx, dy, sx = 0, 0, 0
 
-		if (btn(⬆️)) dy -= spd
-		if (btn(⬇️)) dy += spd
-		if (btn(⬅️)) dx -= spd
-		if (btn(➡️)) dx += spd
+		if (btn(⬆️)) dy -= 1
+		if (btn(⬇️)) dy += 1
+		if (btn(⬅️)) dx -= 1
+		if (btn(➡️)) dx += 1
 		if (btn(🅾️)) fire(_ENV)
-
 		if dx != 0 or dy != 0 then
 			-- normalize movement
 			local a = atan2(dx, dy)
-			sx = cos(a)
-			y += sin(a)
+
+			sx = cos(a) * spd
+			y += sin(a) * spd
 
 			x = flr(x + sx) + 0.5
 			y = flr(y) + 0.5
-
-			sprtTemp += (sx - sprtTemp) * 0.15
 
 			-- spawn dust each 3/10 sec
 			-- if (t()*10)\1%3==0 then
@@ -61,6 +58,9 @@ ship = entity:extend({
 		x = mid(7, x, 114)
 		y = mid(15, y, 116)
 
+		sprt += (sx - sprt) * 0.15
+		sprt = mid(-1, sprt, 1)
+
 		if shotTemp > 0 then
 			shotTemp -= 1
 		end
@@ -71,6 +71,6 @@ ship = entity:extend({
 		-- pset(x,y,7)
 		print(#shot.pool, 5, 5, 7)
 		print(sx)
-		print(sprtTemp)
+		print(sprt)
 	end
 })
